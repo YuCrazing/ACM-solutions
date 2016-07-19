45d95527e9bb9f9e3ba950d12b60774cf7e49a45
YuChang <YuChang@mail.bnu.edu.cn>
2016-07-20 01:34:55 +0800

    -

--- Codeforces/Codeforces Round #363 (Div. 2)/B. One Bomb.cpp
+++ Codeforces/Codeforces Round #363 (Div. 2)/B. One Bomb.cpp
@@ -4,46 +4,33 @@
 using namespace std;
 
 char mp[1111][1111];
-vector<pair<int, int> > g;
+int row[1111], col[1111], sum;
 int main() {
     int n, m;
     scanf("%d%d", &n, &m);
     for(int i = 0; i < n;  i++) {
         scanf("%s", mp[i]);
         for(int j = 0; j < m; j ++)
-            if(mp[i][j] == '*') g.push_back(make_pair(i, j));
-    }
-
-    int sz = g.size();
-    bool ok = 1;
-    int x, y;
-    x = y = -1;
-    if(sz > n + m - 1) ok = 0;
-    if(ok) {
-        if(sz == 0){
-            x = y = 0;
-        }
-        else if(sz == 1) {
-            x = g[0].first;
-            y = g[0].second;
-        } else {
-            ok = 0;
-            for(int i = 0; i < sz; i ++) {
-                int u = g[i].first;
-                int v = g[i].second;
-                int cnt = 0;
-                for(int j = 0; j < n; j ++) if(mp[j][v] == '*') cnt ++;
-                for(int j = 0; j < m; j ++) if(mp[u][j] == '*') cnt ++;
-                if(cnt == sz + 1){
-                    ok = 1;
-                    x = u;
-                    y = v;
-                    break;
-                }
+            if(mp[i][j] == '*') {
+                row[i] ++;
+                col[j] ++;
+                sum ++;
             }
-        }
-
     }
+
+    bool ok = 0;
+    int x, y;
+    for(int i = 0; i < n; i ++)
+    for(int j = 0; j < m; j ++){
+        int t = row[i] + col[j];
+        if(mp[i][j] == '*') t --;
+        if(t == sum){
+            x = i;
+            y = j;
+            ok = 1;
+        }
+    }
+
     if(ok) printf("YES\n%d %d\n", x + 1, y + 1);
     else printf("NO\n");
 
