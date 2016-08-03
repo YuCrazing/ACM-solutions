b85863b0872bde4a24708fd0dd79413444815ee6
YuChang <YuChang@mail.bnu.edu.cn>
2016-08-03 20:13:41 +0800

    Expacted Value

---
+++ LightOJ/1027 - A Dangerous Maze.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T, n, x;
+    scanf("%d", &T);
+    for(int cas = 1; cas <= T; cas ++) {
+        scanf("%d", &n);
+        int p = 0;
+        int sum = 0;
+        for(int i = 0; i < n; i ++) {
+            scanf("%d", &x);
+            if(x < 0) sum += -x;
+            else {
+                sum += x;
+                p ++;
+            }
+        }
+
+        printf("Case %d: ", cas);
+        if(!p) {
+            printf("inf\n");
+            continue;
+        }
+        int g = __gcd(sum, p);
+        printf("%d/%d\n", sum / g, p / g);
+
+    }
+    return 0;
+}
