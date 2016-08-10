02df8d75381e1f735273813573f3cd464fcacba2
YuChang <YuChang@mail.bnu.edu.cn>
2016-08-10 15:27:18 +0800

    :star:  Matrix exponentiation

---
+++ LightOJ/1064 - Throwing Dice.cpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+#include <algorithm>
+#define mp make_pair
+#define pp first
+#define pq second
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+pll dp[25][25*6 + 3];
+
+void init() {
+    for(int i = 0; i < 25; i ++) {
+        dp[i][0] = mp(0, 1);
+        for(int j = 1; j < 25*6 + 3; j ++)
+            dp[i][j] = mp(1, 1);
+    }
+    ll p, q, p1, p2, q1, q2, g;
+    for(int i = 1; i < 25; i ++) {
+        for(int j = 1; j <= 6 * i; j ++) {
+            p1 = 0;
+            q1 = 1;
+            for(int k = 1; k <= 6; k ++) {
+                if(j < k) continue;
+                p2 = dp[i - 1][j - k].pp;
+                q2 = dp[i - 1][j - k].pq;
+                g = __gcd(q1, q2);
+
+                q = q1 / g * q2;
+                p = q2 / g * p1 + q1 / g * p2;
+
+                g = __gcd(p, q);
+                p /= g;
+                q /= g;
+
+                p1 = p;
+                q1 = q;
+            }
+            g = __gcd(p1, 6LL);
+            p1 /= g;
+            q1 *= 6 / g;
+            dp[i][j] = mp(p1, q1);
+        }
+    }
+}
+
+int main() {
+
+    init();
+
+    int T, n, x;
+    scanf("%d", &T);
+    for(int cas = 1; cas <= T; cas ++) {
+        scanf("%d%d", &n, &x);
+
+        ll q = dp[n][x].pq;
+        ll p = q - dp[n][x].pp;
+
+        printf("Case %d: ", cas);
+        if(q == 1) printf("%lld\n", p);
+        else printf("%lld/%lld\n", p, q);
+    }
+    return 0;
+}
--- LightOJ/1065 - Number Sequence.cpp
+++ LightOJ/1065 - Number Sequence.cpp
@@ -1,10 +1,11 @@
 #include <cstdio>
 #include <cstring>
 using namespace std;
-typedef unsigned long long ull;
 
+int mod;
 struct Matrix {
-    ull m[2][2];
+
+    int m[2][2];
     int r, c;
 
     Matrix(int _r = 2, int _c = 2):r(_r),c(_c) {
@@ -12,19 +13,19 @@ struct Matrix {
         for(int i = 0; i < r; i ++) m[i][i] = 1;
     }
 
-    Matrix operator *(const Matrix& B)const {
+    Matrix operator * (const Matrix& B)const {
         Matrix C(r, B.c);
         for(int i = 0; i < r; i ++)
             for(int j = 0; j < B.c; j ++) {
                 C.m[i][j] = 0;
-                for(int k = 0; k < c; k ++) C.m[i][j] += m[i][k] * B.m[k][j];
+                for(int k = 0; k < c; k ++)
+                    C.m[i][j] = (C.m[i][j] + m[i][k] * B.m[k][j] % mod) % mod;
             }
         return C;
     }
-
 };
 
-Matrix qmod(Matrix A, ull b) {
+Matrix qmod(Matrix A, int b) {
     Matrix C(A.r, A.c);
     while(b) {
         if(b & 1) C = C * A;
@@ -33,24 +34,36 @@ Matrix qmod(Matrix A, ull b) {
     }
     return C;
 }
+
+int p[5];
+void init() {
+    p[0] = 1;
+    for(int i = 1; i < 5; i ++) p[i] = 10 * p[i - 1];
+}
+
 int main() {
-    int T;
-    ull p, q, n;
+
+    init();
+
+    int T, a, b, n, m;
     scanf("%d", &T);
     for(int cas = 1; cas <= T; cas ++) {
-        scanf("%llu%llu%llu", &p, &q, &n);
+        scanf("%d%d%d%d", &a, &b, &n, &m);
+        mod = p[m];
+        a %= mod;
+        b %= mod;
         Matrix k(2, 2), A(2, 1);
-        k.m[0][0] = p;
-        k.m[0][1] = -q;
+        k.m[0][0] = 1;
+        k.m[0][1] = 1;
         k.m[1][0] = 1;
         k.m[1][1] = 0;
 
-        A.m[0][0] = p;
-        A.m[1][0] = 2;
+        A.m[0][0] = b;
+        A.m[1][0] = a;
 
         k = qmod(k, n);
         A = k * A;
-        printf("Case %d: %llu\n", cas, (n == 0 ? 2 : A.m[1][0]));
+        printf("Case %d: %d\n", cas, (n == 0 ? a : A.m[1][0]));
     }
     return 0;
 }
