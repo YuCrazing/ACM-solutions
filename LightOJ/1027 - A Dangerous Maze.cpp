502f6e54b0a47a1b9171ae6b6b239cca93ba1d39
YuChang <723284893@qq.com>
2017-03-20 09:39:45 +0800

    Expected

--- LightOJ/1027 - A Dangerous Maze.cpp
+++ LightOJ/1027 - A Dangerous Maze.cpp
@@ -9,6 +9,7 @@ int main() {
         scanf("%d", &n);
         int p = 0;
         int sum = 0;
+
         for(int i = 0; i < n; i ++) {
             scanf("%d", &x);
             if(x < 0) sum += -x;
