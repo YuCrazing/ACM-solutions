da2ebd4ce01f03d0a26810fba5b1d8304fe980f1
YuChang <723284893@qq.com>
2017-06-02 17:06:28 +0800

    :star: 偏序

---
+++ HDU/1800 Flying to the Mars.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<unordered_map>
+using namespace std;
+
+unordered_map<string, int> mp;
+
+int n;
+string s;
+int main() {
+    while(cin >> n) {
+        mp.clear();
+        for(int i = 0; i < n; i++) {
+            cin >> s;
+            int j = 0;
+            if(j == s.size()) s = "0";
+            else s = s.substr(j);
+            if(mp.find(s) == mp.end()) mp[s] = 1;
+            else mp[s]++;
+        }
+        int ans = 1;
+        for(unordered_map<string, int>::iterator it = mp.begin(); it != mp.end(); it++) {
+            ans = max(ans, it->second);
+        }
+        cout << (n ? ans : 0) << endl;
+    }
+    return 0;
+}
